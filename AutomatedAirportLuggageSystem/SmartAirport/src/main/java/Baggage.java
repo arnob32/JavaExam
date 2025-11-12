f3f344567414e7afac46631f8ccde8aef03def1d
Md Ibrahim khalil <44796932+ibrahimkhalil007@users.noreply.github.com>
2025-11-12 01:08:39 +0100

    Add properties and methods to Baggage class

--- AutomatedAirportLuggageSystem/SmartAirport/src/main/java/Baggage.java
+++ AutomatedAirportLuggageSystem/SmartAirport/src/main/java/Baggage.java
@@ -3,5 +3,19 @@ package SmartAirport.src.main.java;
 
 
 public class Baggage {
-   
+    private final int id;
+    private final String destination;
+    private String status = "Pending";
+
+    public Baggage(int id, String destination) {
+        this.id = id;
+        this.destination = destination;
+    }
+
+    public int getId() { return id; }
+    public String getDestination() { return destination; }
+
+    public void updateStatus(String newStatus) { this.status = newStatus; }
+    public String getStatus() { return status; }
 }
+
